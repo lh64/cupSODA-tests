d44de71312e9f1d79e935c47cd410ceab5698110
jamespino <james.c.pino@vanderbilt.edu>
2015-05-21 16:12:23 -0500

    updated the file to run cupSODA testing

--- compare_cupSODA_scipy_lsoda.py
+++ compare_cupSODA_scipy_lsoda.py
@@ -4,31 +4,42 @@ Created on Tue Mar 24 22:00:07 2015
 
 @author: James C. Pino
 """
-import time   
+import time
+import pysb.integrate2
 import pysb.integrate
+import pysb
 import numpy as np
 import pylab as plt
 import os
 from pysb.tools.cupsoda import *
 from pysb.bng import generate_equations
 import sys
-
-if sys.argv[1] == 'ras':
+#run = sys.argv[1]
+name = sys.argv[1]
+#name = 'tyson'
+if name == 'ras':
     from ras_amp_pka import model
-    tspan = np.linspace(0,1500,1000)
-if sys.argv[1] == 'earm':
+    tspan = np.linspace(0,1500,100)
+elif name == 'earm':
     from earm.lopez_embedded import model
-    tspan = np.linspace(0, 20000,1000)
-if sys.argv[1] == 'tyson':
+    tspan = np.linspace(0, 20000,100)
+elif name == 'tyson':
     from pysb.examples.tyson_oscillator import model
     tspan = np.linspace(0,100,100)
 
+#run = 'scipy'
+run ="cupSODA"
+
 generate_equations(model)
 
-#tspan = np.linspace(0, 20000,1000)
-#tspan = np.linspace(0, 100,100)
-#tspan = np.linspace(0,1500,1000)
-set_cupSODA_path("/home/pinojc/CUPSODA")
+ATOL = 1e-6
+RTOL = 1e-6
+mxstep=20000
+det = 1
+vol = 0
+card = 'K20c'
+#card = 'gtx760'
+
 
 
 params_names = [p.name for p in model.parameters]
@@ -47,67 +58,70 @@ par_dict = {par_names[i] : i for i in range(len(par_names))}
 par_vals = np.array([model.parameters[nm].value for nm in par_names])
 
 
-run ="cupSODA"
-
-
-def main(number_particles,n_blocks):
-    n_blocks = n_blocks
+global output
+output = "model,nsims,tpb,mem,pythontime,rtol,atol,mxsteps,t_end,n_steps,deterministic,vol,card\n"
+def main(number_particles):
+    global output
     num_particles = int(number_particles)
     if run == "cupSODA":
         c_matrix = np.zeros((num_particles, len(model.reactions)))
         rate_args = []
         for rxn in model.reactions:
-            #print rxn['rate']
             rate_args.append([arg for arg in rxn['rate'].args if not re.match("_*s",str(arg))])
         for j in range(len(model.reactions)):
             rate = 1.0
             for r in rate_args[j]:
                 x = str(r)
                 if x in par_dict.keys():
-                    rate *= par_vals[par_dict[x]] # model.parameters[x].value
+                    rate *= par_vals[par_dict[x]]
                 else:
                     rate *= float(x)
-            c_matrix[:,j] = rate        
-        
-        #print par_vals - c_matrix
-        # Initial concentrations
+            c_matrix[:,j] = rate
+
         MX_0 = np.zeros((num_particles,len(model.species)))
         for i in xrange(len(model.initial_conditions)):
             for j in xrange(len(model.species)):
                 if str(model.initial_conditions[i][0]) == str(model.species[j]): # The ComplexPattern objects are not the same, even though they refer to the same species (ask about this)
                     x = model.initial_conditions[i][1]
                     MX_0[:,j] = [x.value for each in xrange(num_particles)]
-                    break  
-        solver = CupSODASolver(model, tspan, atol=1e-6, rtol=1e-6, verbose=False)
-        Start = time.time()
-        solver.run(c_matrix, MX_0 , n_blocks = n_blocks, \
-                   outdir=os.path.join('.','CUPSODA_%s') % model.name, gpu=2, load_conc_data=False) 
-        print 'sim = %s ,theads/block= %s, time = %s sec' % (num_particles,num_particles/n_blocks,time.time() - Start)
-        for i in solver.y[0]:
-            print i,
+                    break
+        for mem in [0,1,2]:
+            for i in  (1,2,4,8,16,32,64):
+                if num_particles < i :
+                    continue
+                if i == 64 and name == 'earm':
+                    continue
+                solver = CupSODASolver(model, tspan, atol=ATOL, rtol=RTOL, verbose=True)
+                Start = time.time()
+                solver.run(c_matrix, MX_0 , n_blocks = np.int(num_particles/i), \
+                outdir=os.path.join('.','CUPSODA_%s')%model.name, gpu=2,max_steps=mxstep,load_conc_data=False,memory_usage=mem)
+                output +='%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'% \
+                (name,num_particles,str(i),mem,time.time()-Start,RTOL,ATOL,len(tspan),mxstep,np.max(tspan),det,vol,card)
+                print name,num_particles,str(i),mem,time.time()-Start,RTOL,ATOL,len(tspan),np.max(tspan),det,vol,card
+                print 'out==',solver.yobs[0][0],solver.yobs[0][-1],'==out'
+                os.system('rm -r %s'%os.path.join('.','CUPSODA_%s')%model.name)
+                print 'removed directory'
+
     if run =="scipy":
-        solver = pysb.integrate.Solver(model, tspan, integrator='lsoda', \
-                                       rtol=1e-6, atol=1e-6, nsteps=10000)
+        solver = pysb.integrate2.Solver(model, tspan,rtol=1e-6, atol=1e-6, integrator='lsoda', mxstep=mxstep)
+        Time = np.zeros(number_particles)
         Start = time.time()
-        for i in xrange(num_particles):
-            solver.run()
-        print 'sim = %s , time = %s sec' % (num_particles,time.time() - Start)
-if run =='scipy':                
-    #main(1e1,1)
-    #main(1e2,1)
-    main(1e3,1)
-    main(1e4,1)
-    main(1e5,1)
+        for i in xrange(number_particles):
+            Time[i] = solver.run()
+        print 'sim = %s , time = %s sec' % (number_particles,time.time() - Start)
+        print 'time of solver itself',np.sum(Time)
+if run =='scipy':
+    for j in (10,100,1000,10000,100000):
+        main(j)
 if run == 'cupSODA':
-    #for i in (32,64,128,256,512,1024,None):
-    for i in (8,10,20,25,32,40,50):
-        #main(1,i)
-        #main(1e1,i)
-        #main(1e2,i)
-        #main(1e3,np.round(1e3/i))
-        main(1e4,np.round(1e4/i))
-        main(1e5,np.round(1e5//i))
-        main(1e6,np.round(1e6/i))    
+    set_cupSODA_path("/home/pinojc/CUPSODA")
+    for j in (10,100,1000,10000,100000):
+        main(j)
+    outFile = open(sys.argv[2],'w')
+    outFile.write(output)
+    outFile.close()
+    quit()
+
 
 
 
