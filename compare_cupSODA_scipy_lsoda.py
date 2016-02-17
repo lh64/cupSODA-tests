587ae4234ac771040764a015bf8b54d2f4730322
jamespino <james.c.pino@vanderbilt.edu>
2016-02-17 13:24:21 -0600

    Updated creation of paramater matrix

--- compare_cupSODA_scipy_lsoda.py
+++ compare_cupSODA_scipy_lsoda.py
@@ -1,60 +1,57 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 24 22:00:07 2015
-
-@author: James C. Pino
-"""
 import time
 import pysb.integrate as integrate
 import pysb
 import numpy as np
-import pylab as plt
 import re
 import os
-from pysb_cupsoda import set_cupsoda_path,CupsodaSolver
+from pysb_cupsoda import set_cupsoda_path, CupsodaSolver
 from pysb.bng import generate_equations
 import sys
 import multiprocessing
 
-#name = sys.argv[1]
-name = 'tyson'
+name = sys.argv[1]
+#name = 'tyson'
 if name == 'ras':
     from ras_amp_pka import model
-    tspan = np.linspace(0,1500,100)
-    simulations = [10,100,1000,10000,100000]
+
+    tspan = np.linspace(0, 1500, 100)
+    simulations = [10, 100, 1000, 10000, 100000]
 elif name == 'tyson':
     from pysb.examples.tyson_oscillator import model
-    tspan = np.linspace(0,100,100)
-    simulations = [10,100,1000,]#10000,100000]
+    tspan = np.linspace(0, 100, 100)
+    simulations = [10, 100, 1000, 10000, 100000]
+    simulations = [10, 100, 1000]
 elif name == 'earm':
     from earm.lopez_embedded import model
-    tspan = np.linspace(0, 20000,100)
-    simulations = [10,100,1000,10000]
+    tspan = np.linspace(0, 20000, 100)
+    #simulations = [ 1000, 5000, 10000, 15000]
+    simulations = [10, 100, 1000, 10000]#,100000]
 
 run = 'cupSODA'
 multi = False
-#run ="cupSODA"
+run ="scipy"
 
 generate_equations(model)
 
 ATOL = 1e-6
 RTOL = 1e-6
-mxstep=20000
+mxstep = 20000
 det = 1
 vol = 0
-card = 'K20c'
-#card = 'gtx760'
+# card = 'K20c'
+# card = 'gtx760'
+card = 'gtx980-diablo'
 
-#puma
+# puma
 CPU = 'Intel-Xeon-E5-2687W-v2'
 GHZ = '3.40'
 
-#macbook
-#CPU = 'Intel-core-I5'
-#GHZ = '2.5'
+# macbook
+# CPU = 'Intel-core-I5'
+# GHZ = '2.5'
 
 
-params_names = [p.name for p in model.parameters]
+params_names = [p.name for p in model.parameters_rules()]
 init_name = [p[1].name for p in model.initial_conditions]
 par_names = []
 for parm in params_names:
@@ -65,95 +62,113 @@ for parm in params_names:
 rate_params = model.parameters_rules()
 rate_mask = np.array([p in rate_params for p in model.parameters])
 nominal_values = np.array([p.value for p in model.parameters])
-xnominal = np.log10(nominal_values[rate_mask])
-par_dict = {par_names[i] : i for i in range(len(par_names))}
-par_vals = np.array([model.parameters[nm].value for nm in par_names])
+par_dict = {par_names[i]: i for i in range(len(par_names))}
 
-if run =='scipy':
+# quit()
+if run == 'scipy':
     global output
     output = "model,nsims,scipytime,rtol,atol,mxsteps,t_end,n_steps,cpu,GHz,num_cpu\n"
-    #global output
-    solver = pysb.integrate.Solver(model, tspan,rtol=RTOL, atol=ATOL, integrator='lsoda', )
-    def RUN(params):
+    solver = pysb.integrate.Solver(model, tspan, rtol=RTOL, atol=ATOL, integrator='lsoda', )
+
+
+    def run_pool(params):
         solver.run(params)
 if run == 'cupSODA':
     set_cupsoda_path("/home/pinojc/git/cupSODA")
-    output = "model,nsims,tpb,mem,pythontime,rtol,atol,mxsteps,t_end,n_steps,deterministic,vol,card\n"
-    #global output
+    solver = CupsodaSolver(model, tspan, atol=ATOL, rtol=RTOL, verbose=False)
+    output = "model,nsims,tpb,mem,cupsodatime,pythontime,rtol,atol,mxsteps,t_end,n_steps,deterministic,vol,card\n"
+
 
 def main(number_particles):
     num_particles = int(number_particles)
+    c_matrix = np.zeros((num_particles, len(nominal_values)))
+    c_matrix[:, :] = nominal_values
     global output
     if run == "cupSODA":
-        c_matrix = np.zeros((num_particles, len(model.reactions)))
-        rate_args = []
-        for rxn in model.reactions:
-            rate_args.append([arg for arg in rxn['rate'].args if not re.match("_*s",str(arg))])
-        for j in range(len(model.reactions)):
-            rate = 1.0
-            for r in rate_args[j]:
-                x = str(r)
-                if x in par_dict.keys():
-                    rate *= par_vals[par_dict[x]]
-                else:
-                    rate *= float(x)
-            c_matrix[:,j] = rate
-
-        MX_0 = np.zeros((num_particles,len(model.species)))
+        mx_0 = np.zeros((num_particles, len(model.species)))
         for i in xrange(len(model.initial_conditions)):
             for j in xrange(len(model.species)):
-                if str(model.initial_conditions[i][0]) == str(model.species[j]): # The ComplexPattern objects are not the same, even though they refer to the same species (ask about this)
+                if str(model.initial_conditions[i][0]) == str(model.species[j]):
                     x = model.initial_conditions[i][1]
-                    MX_0[:,j] = [x.value for each in xrange(num_particles)]
+                    mx_0[:, j] = x.value
                     break
-        for mem in [0,1,2]:
-            for i in  (1,2,4,8,16,32,64):
-                if num_particles < i :
-                    continue
-                if i == 64 and name == 'earm':
-                    continue
-                solver = CupsodaSolver(model, tspan, atol=ATOL, rtol=RTOL, verbose=False)
-                Start = time.time()
-                solver.run(c_matrix, MX_0 , n_blocks = np.int(num_particles/i), \
-                outdir=os.path.join('.','CUPSODA_%s')%model.name, gpu=2,max_steps=mxstep,load_conc_data=False,memory_usage=mem)
-                output +='%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'% \
-                (name,num_particles,str(i),mem,time.time()-Start,RTOL,ATOL,len(tspan),mxstep,np.max(tspan),det,vol,card)
-                print name,num_particles,str(i),mem,time.time()-Start,RTOL,ATOL,len(tspan),np.max(tspan),det,vol,card
-                print 'out==',solver.yobs[0][0],solver.yobs[0][-1],'==out'
-                os.system('rm -r %s'%os.path.join('.','CUPSODA_%s')%model.name)
-                print 'removed directory'
+        #for mem in [0, 1, 2]:
+        #    for i in (1, 2, 4, 8, 16, 32, 64):
+        #        if num_particles < i:
+        #            continue
+        #        if i == 64 and name == 'earm':
+        #            continue
+        mem = 2
+        i = 16
+        start_time = time.time()
+        solver.run(c_matrix,
+                   mx_0,
+                   n_blocks=np.int(num_particles / i),
+                   outdir=os.path.join('/tmp/ramdisk', 'CUPSODA_%s') % model.name,
+                   gpu=0,
+                   max_steps=mxstep,
+                   load_conc_data=False,
+                   memory_usage=mem)
+        end_time = time.time()
+        new_line = '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % \
+                  (name, num_particles, str(i), mem,solver.time, end_time - start_time, RTOL, ATOL, len(tspan), mxstep,
+                   np.max(tspan), det, vol, card)
+        output += new_line
+        print new_line
+        """
+        t = tspan
+        x = solver.yobs["YT"]
+
+        import pylab as plt
+        plt.plot(t, solver.yobs["YT"][0,:], lw=2, label='YT')
+        plt.plot(t, solver.yobs["M"][0,:], lw=2, label='M')
+
+        plt.legend(loc=0)
+        plt.xlabel('time')
+        plt.ylabel('population')
+
+        plt.show()
+        """
+        print 'out==', solver.yobs[0][0], solver.yobs[0][-1], '==out'
+        os.system('rm -r %s' % os.path.join('/tmp/ramdisk', 'CUPSODA_%s') % model.name)
+        print 'removed directory'
 
     if run == 'scipy':
 
-        #global solver
-
-        c_matrix = np.zeros((num_particles, len(nominal_values)))
-        c_matrix[:,:] = nominal_values
         pool = multiprocessing.Pool(processes=num_processes)
-        Start = time.time()
-        pool.map(RUN,c_matrix)
-        TIME = time.time() - Start
-        print 'sim = %s , time = %s sec' % (number_particles,TIME)
-        output +='%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'% \
-                (name,num_particles,TIME,RTOL,ATOL,mxstep,np.max(tspan),len(tspan),CPU,GHZ,str(num_processes))
+        start_time = time.time()
+        #pool.map(run_pool, c_matrix)
+        for i in xrange(number_particles):
+            solver.run()
+        total_time = time.time() - start_time
+        print 'sim = %s , time = %s sec' % (number_particles, total_time)
+        output += '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % \
+                  (name,
+                   num_particles,
+                   total_time,
+                   RTOL,
+                   ATOL,
+                   mxstep,
+                   np.max(tspan),
+                   len(tspan),
+                   CPU,
+                   GHZ,
+                   str(num_processes))
 
-if multi == True:
-    for i in [1,2,4]:#,8,16,32]:
+
+if multi:
+    for i in [1, 2, 4, 8, 16, 32]:
         print i
         num_processes = i
         for j in simulations:
             main(j)
 else:
     num_processes = 1
+    #main(10)
     for j in simulations:
         main(j)
-quit()
 print output
-outFile = open(sys.argv[2],'w')
+outFile = open(sys.argv[2], 'w')
 outFile.write(output)
 outFile.close()
-
-
-
-
 
